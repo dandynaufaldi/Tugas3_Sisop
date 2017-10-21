7b233c95228f5756858d76f94069499e443cb6f5
nawamkihafahd <nawamkihafahd@gmail.com>
2017-10-21 13:27:08 +0700

    Prototype soal1->sequential execution thread

--- Latihan1/soal1.c
+++ Latihan1/soal1.c
@@ -8,6 +8,48 @@ pthread_t tid[2];
 char string[200];
 
 int wait=1;
+
+void* copy(void *arg)
+{
+    unsigned long i=0;
+    pthread_t id=pthread_self();
+    int iter;
+    if(pthread_equal(id,tid[0]))
+    {
+        FILE* inpu=fopen("baca.txt","r");
+	FILE* outp=fopen("salin1.txt","w");
+	if(inpu !=NULL)
+	{
+		while(fgets(string, 199, inpu)!=NULL)
+		{
+			fputs(string,outp);
+		}
+		fclose(inpu);
+		wait = 0;
+	}
+	else
+	printf("Cannot access baca.txt");
+	fclose(outp);
+    }
+    else if(pthread_equal(id,tid[1]))
+    {
+        while(wait);
+	FILE* inp=fopen("salin1.txt","r");
+	FILE* out=fopen("salin2.txt", "w");
+	if(inp !=NULL)
+	{
+		while(fgets(string, 199, inp)!=NULL)
+		{
+			fputs(string,out);
+		}
+		fclose(inp);
+	}
+	else
+	printf("Cannot access salin1.txt");
+	fclose(out);
+    }
+    return NULL;
+}
 int main(void)
 {
     int i=0;
